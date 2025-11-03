6417fc97173929c3e8b3f009ffea804b0ff5a104
SenhorDosSonhos1 <marcelosamu2002@gmail.com>
2025-11-03 08:32:26 -0300

    chore: configurar diretórios de mídia e ajustar exibição dos arquivos no projeto

--- galeria/models.py
+++ galeria/models.py
@@ -1,11 +1,21 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+CHOICES = [
+    ("NEBULOSA", "Nebulosa"),
+    ("ESTRELA", "Estrela"),
+    ("GALAXIA", "Galáxia"),
+    ("PLANETA", "Planeta")
+]
+
 class Fotografia(models.Model):
     nome = models.CharField(max_length = 100, null = False, blank = False)
     legenda = models.CharField(max_length = 155, null=False, blank=False)
+    categoria = models.CharField(max_length=155, choices=CHOICES, default="")
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length = 100, null = False, blank = False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
-        return f'Fotografia: [nome={self.nome}]'
+        return self.nome
