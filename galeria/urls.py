de1282d9e0ca8352ee2793d817658b373189e2ce
SenhorDosSonhos1 <marcelosamu2002@gmail.com>
2025-11-04 08:50:18 -0300

    add: adicionando funcionalidade de buscas

--- galeria/urls.py
+++ galeria/urls.py
@@ -3,5 +3,6 @@ from galeria import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('imagem/<int:id>', views.imagem, name='imagem')
+    path('imagem/<int:id>', views.imagem, name='imagem'),
+    path('buscar/', views.buscar, name='buscar')
 ]
